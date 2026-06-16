c3bda1fc0d07f46055605f8e7ae27e6a0178630b
Simon Jeker <jekersim@students.zhaw.ch>
2026-06-17 00:20:11 +0200

    feat: add structure for test taking

---
+++ HM1-SEP/AufgabeY.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+# region header
+"""
+========================================================================================================================
+SEP:      Höhere Mathematik für Informatiker 1
+Semester: HS 20XX
+Aufgabe:  Y
+========================================================================================================================
+Author:   XXXXXX
+Klasse:   XXXXXX
+Datum:    DD.MM.YYYY HH:MM
+========================================================================================================================
+Aufbau Abgabefile:
+- Header mit Metainformationen
+- Imports
+- 5 Regionen pro Teilaufgabe:
+    1) Part 1 — Inputs (Variablen, Funktionen, Parameter)
+    2) Part 2 — Method selection (Auswahl der Variante, falls mehrere möglich)
+    3) Part 3 — Implementation (eine benannte Funktion)
+    4) Part 4 — Call (Aufruf der Implementation)
+    5) Comments (Antworten / Begründungen / Resultate)
+========================================================================================================================
+"""
+# endregion
+
+# region imports
+
+# endregion
+
+# region Teilaufgaben
+
+# ======================================================================================================================
+# Aufgabe Y. a)
+# ======================================================================================================================
+
+# region a) Part 1 — Inputs
+
+# endregion
+
+# region a) Part 2 — Method selection
+
+# endregion
+
+# region a) Part 3 — Implementation
+
+# endregion
+
+# region a) Part 4 — Call
+
+# endregion
+
+# region a) Comments ANTWORTEN SIND HIER!!!
+"""
+
+"""
+# endregion
+
+# ======================================================================================================================
+# Aufgabe Y. b)
+# ======================================================================================================================
+
+# region b) Part 1 — Inputs
+
+# endregion
+
+# region b) Part 2 — Method selection
+
+# endregion
+
+# region b) Part 3 — Implementation
+
+# endregion
+
+# region b) Part 4 — Call
+
+# endregion
+
+# region b) Comments ANTWORTEN SIND HIER!!!
+"""
+
+"""
+# endregion
+
+# ======================================================================================================================
+# Aufgabe Y. c)
+# ======================================================================================================================
+
+# region c) Part 1 — Inputs
+
+# endregion
+
+# region c) Part 2 — Method selection
+
+# endregion
+
+# region c) Part 3 — Implementation
+
+# endregion
+
+# region c) Part 4 — Call
+
+# endregion
+
+# region c) Comments ANTWORTEN SIND HIER!!!
+"""
+
+"""
+# endregion
+
+# endregion
